0726a4b5b5327609e2e74ab8ecc0bdf9eebb74dc
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-02-22 14:15:15 +0530

    condition

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,46 +1,35 @@
 import { useState, useEffect } from "react";
-import { projectAuth } from "../firebase/config.js";
-import { useAuthContext } from "./useAuthContext.js";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
 
-export const useSignup = () => {
+export const useLogin = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
-
   const { dispatch } = useAuthContext();
 
-  const signup = async (email, password, displayName) => {
+  const login = async (email, password) => {
     setError(null);
     setIsPending(true);
 
     try {
-      const res = await projectAuth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-
-      if (!res) {
-        throw new Error("Could not complete signup");
-      }
-
-      await res.user.updateProfile({ displayName: displayName });
+      const res = await projectAuth.signInWithEmailAndPassword(email, password);
 
       dispatch({ type: "LOGIN", payload: res.user });
+
       if (!isCancelled) {
         setIsPending(false);
         setError(null);
       }
     } catch (err) {
       if (!isCancelled) {
-        console.log(err.message);
         setError(err.message);
         setIsPending(false);
       }
     }
   };
-
   useEffect(() => {
     return () => setIsCancelled(true);
   }, []);
-  return { error, isPending, signup };
+  return { login, error, isPending };
 };
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,15 @@
 import styles from "./Login.module.css";
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
   return (
     <form onSubmit={handleSubmit} className={styles["login-form"]}>
@@ -27,7 +30,13 @@ export default function Login() {
           value={password}
         />
       </label>
-      <button className="btn">Login</button>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <p>error</p>}
     </form>
   );
 }
